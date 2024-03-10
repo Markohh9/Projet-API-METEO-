d43a5bdc3c9aa39cba6c13da485b35a509e97392
Marco <zehow.zhw@gmail.com>
2024-03-10 02:18:22 +0100

    adding the function getWeatherType to attribute weather to weatherIconCode

--- js/index.js
+++ js/index.js
@@ -28,37 +28,102 @@ const updateUI = (data) => {
         const formatter = new Intl.DateTimeFormat('fr-FR', timeOptions);
         return formatter.format(currentTime);
     }
-getLocalTime();
+    getLocalTime();
+
+    // Get Time of city //
+    const cityTimeZone = cityDets.TimeZone.Name;
+    const localTime = getLocalTime(cityTimeZone);
+    console.log("Local time:", localTime);
+
+    // defined the night/day/twilight //
+    const getTimeOfDay = (localTime) => {
+        const hour = parseInt(localTime.split(':')[0]); // Extraire l'heure de la chaîne de caractères
+
+        if (hour >= 8 && hour < 18) {
+            return "day";
+        } else if ((hour >= 6 && hour < 8) || (hour >= 18 && hour < 21)) {
+            return "twilight";
+        } else {
+            return "night";
+        }
+    };
+
+    console.log("Time of day:", getTimeOfDay(localTime));
 
 
-    // change String to date the time of the city // 
+//
+
+    // Get weather icon code
+    const weatherIconCode = weather.WeatherIcon;
+    console.log("Weather icon code:", weatherIconCode);
+
+    // Atribute WeatherIconCode to Weather
+const getWeatherType = (weatherIconCode) => {
+
+    const weatherTypes = {
+        'Soleil': [1, 2, 3, 4, 30, 33, 34],
+        'Nuageux': [5, 6, 7, 38],
+        'Pluie': [12, 13, 14, 18, 39, 40],
+        'Vent': [32, 35, 36],
+        'Brume': [8, 11, 37],
+        'Neige': [22, 23, 24, 25, 26, 29, 44],
+        'Tempete': [15, 16, 17, 19, 20, 21, 31, 41, 42, 43]
+    };
+
+     // Search for the code of the weatherTypes
+    for (const [weatherType, codes] of Object.entries(weatherTypes)) {
+        if (codes.includes(weatherIconCode)) {
+            return weatherType;
+        }
+    }
+    return 'Autre';
+};
+// get the weathertype with the weathercode
+const weatherType = getWeatherType(weatherIconCode);
+console.log("Type de météo:", weatherType);
+
+
+// Changing Background with hour and weather //
 
 
 
-// Get Time of city //
-const cityTimeZone = cityDets.TimeZone.Name;
-const localTime = getLocalTime(cityTimeZone);
-console.log("Local time:", localTime);
-
-// Get weather icon code
-const weatherIconCode = weather.WeatherIcon;
-console.log("Weather icon code:", weatherIconCode);
-
-// defined the night/day/twilight //
-const getTimeOfDay = (localTime) => {
-    const hour = parseInt(localTime.split(':')[0]); // Extraire l'heure de la chaîne de caractères
-
-    if (hour >= 8 && hour < 18) {
-        return "Jour";
-    } else if ((hour >= 6 && hour < 8) || (hour >= 18 && hour < 21)) {
-        return "Crépuscule";
-    } else {
-        return "Nuit";
+const backgroundImages = {
+    'day': {
+        'Soleil': '../img/weather/sun-day.gif',
+        'Nuageux': '../img/weather/cloud-day.gif',
+        'Pluie': '../img/weather/rain-day.gif',
+        'Vent': '../img/weather/wind-day.gif',
+        'Brume': '../img/weather/fog-day.gif',
+        'Neige': '../img/weather/snow-day.gif',
+        'Tempete': '../img/weather/storm-day.gif'
+    },
+    'twilight': {
+        'Soleil': '../img/weather/sun-twilight.gif',
+        'Nuageux': '../img/weather/cloud-twilight.gif',
+        'Pluie': '../img/weather/rain-twilight.gif',
+        'Vent': '../img/weather/wind-twilight.gif',
+        'Brume': '../img/weather/fog-twilight.gif',
+        'Neige': '../img/weather/snow-twilight.gif',
+        'Tempete': '../img/weather/storm-twilight.gif'
+    },
+    'night': {
+        'Soleil': '../img/weather/sun-night.gif',
+        'Nuageux': '../img/weather/cloud-night.gif',
+        'Pluie': '../img/weather/rain-night.gif',
+        'Vent': '../img/weather/wind-night.gif',
+        'Brume': '../img/weather/fog-night.gif',
+        'Neige': '../img/weather/snow-night.gif',
+        'Tempete': '../img/weather/storm-night.gif'
     }
 };
 
 
-console.log("Time of day:", getTimeOfDay(localTime));
+
+
+
+
+
+
 
 
 
@@ -89,19 +154,19 @@ console.log("Time of day:", getTimeOfDay(localTime));
 
     let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
     time.setAttribute('src', timeSrc);
-    
 
 
-    // remove d-none class if city selected
 
     // remove d-none class if city selected
-if (card.classList.contains('d-none')) {
-    card.classList.remove('d-none');
-}
 
-console.log(data)
+    // remove d-none class if city selected
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
 
-////
+    console.log(data)
+
+    ////
 
 
 
@@ -132,36 +197,3 @@ cityForm.addEventListener('submit', e => {
         .then(data => updateUI(data))
         .catch(error => console.log(error));
 });
-
-// Changing Background with hour and weather //
-
-const backgroundImages = {
-    'day': {
-        'Soleil': '../img/weather/sun-day.gif',
-        'Nuageux': '../img/weather/cloud-day.gif',
-        'Pluie' : '../img/weather/rain-day.gif',
-        'Vent' : '../img/weather/wind-day.gif',
-        'Brume' : '../img/weather/fog-day.gif',
-        'Neige' : '../img/weather/snow-day.gif',
-        'Tempete' : '../img/weather/storm-day.gif'
-    },
-    'twilight': {
-        'Soleil': '../img/weather/sun-twilight.gif',
-        'Nuageux': '../img/weather/cloud-twilight.gif',
-        'Pluie' : '../img/weather/rain-twilight.gif',
-        'Vent' : '../img/weather/wind-twilight.gif',
-        'Brume' : '../img/weather/fog-twilight.gif',
-        'Neige' : '../img/weather/snow-twilight.gif',
-        'Tempete' : '../img/weather/storm-twilight.gif'
-    },
-    'night': {
-        'Soleil': '../img/weather/sun-night.gif',
-        'Nuageux': '../img/weather/cloud-night.gif',
-        'Pluie' : '../img/weather/rain-night.gif',
-        'Vent' : '../img/weather/wind-night.gif',
-        'Brume' : '../img/weather/fog-night.gif',
-        'Neige' : '../img/weather/snow-night.gif',
-        'Tempete' : '../img/weather/storm-night.gif'
-    }
-};
-
