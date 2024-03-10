2a6b0cadfd0d10018d74f3579761bc0cc41fcc6c
Marco <zehow.zhw@gmail.com>
2024-03-10 02:55:21 +0100

    adding the final script to change the background with the timeOfDay and the weatherType before create the function to change the background

--- js/index.js
+++ js/index.js
@@ -48,74 +48,79 @@ const updateUI = (data) => {
         }
     };
 
-    console.log("Time of day:", getTimeOfDay(localTime));
 
-
-//
+    //
 
     // Get weather icon code
     const weatherIconCode = weather.WeatherIcon;
     console.log("Weather icon code:", weatherIconCode);
 
     // Atribute WeatherIconCode to Weather
-const getWeatherType = (weatherIconCode) => {
+    const getWeatherType = (weatherIconCode) => {
 
-    const weatherTypes = {
-        'Soleil': [1, 2, 3, 4, 30, 33, 34],
-        'Nuageux': [5, 6, 7, 38],
-        'Pluie': [12, 13, 14, 18, 39, 40],
-        'Vent': [32, 35, 36],
-        'Brume': [8, 11, 37],
-        'Neige': [22, 23, 24, 25, 26, 29, 44],
-        'Tempete': [15, 16, 17, 19, 20, 21, 31, 41, 42, 43]
+        const weatherTypes = {
+            'Soleil': [1, 2, 3, 4, 30, 33, 34],
+            'Nuageux': [5, 6, 7, 38],
+            'Pluie': [12, 13, 14, 18, 39, 40],
+            'Vent': [32, 35, 36],
+            'Brume': [8, 11, 37],
+            'Neige': [22, 23, 24, 25, 26, 29, 44],
+            'Tempete': [15, 16, 17, 19, 20, 21, 31, 41, 42, 43]
+        };
+
+        // Search for the code of the weatherTypes
+        for (const [weatherType, codes] of Object.entries(weatherTypes)) {
+            if (codes.includes(weatherIconCode)) {
+                return weatherType;
+            }
+        }
+        return 'Autre';
     };
 
-     // Search for the code of the weatherTypes
-    for (const [weatherType, codes] of Object.entries(weatherTypes)) {
-        if (codes.includes(weatherIconCode)) {
-            return weatherType;
+
+    // Changing Background with hour and weather //
+
+    const backgroundImages = {
+        'day': {
+            'Soleil': '../img/weather/sun-day.gif',
+            'Nuageux': '../img/weather/cloud-day.gif',
+            'Pluie': '../img/weather/rain-day.gif',
+            'Vent': '../img/weather/wind-day.gif',
+            'Brume': '../img/weather/fog-day.gif',
+            'Neige': '../img/weather/snow-day.gif',
+            'Tempete': '../img/weather/storm-day.gif'
+        },
+        'twilight': {
+            'Soleil': '../img/weather/sun-twilight.gif',
+            'Nuageux': '../img/weather/cloud-twilight.gif',
+            'Pluie': '../img/weather/rain-twilight.gif',
+            'Vent': '../img/weather/wind-twilight.gif',
+            'Brume': '../img/weather/fog-twilight.gif',
+            'Neige': '../img/weather/snow-twilight.gif',
+            'Tempete': '../img/weather/storm-twilight.gif'
+        },
+        'night': {
+            'Soleil': '../img/weather/sun-night.gif',
+            'Nuageux': '../img/weather/cloud-night.gif',
+            'Pluie': '../img/weather/rain-night.gif',
+            'Vent': '../img/weather/wind-night.gif',
+            'Brume': '../img/weather/fog-night.gif',
+            'Neige': '../img/weather/snow-night.gif',
+            'Tempete': '../img/weather/storm-night.gif'
         }
-    }
-    return 'Autre';
-};
-// get the weathertype with the weathercode
+    };
+    
+    // Get Time of day and weather type
+const timeOfDay = getTimeOfDay(localTime);
 const weatherType = getWeatherType(weatherIconCode);
+
 console.log("Type de météo:", weatherType);
+console.log("Time of day:", getTimeOfDay(localTime));
 
+// Get the path of the background image
+const backgroundImage = backgroundImages[timeOfDay][weatherType];
+console.log("Chemin du fond d'écran:", backgroundImage);
 
-// Changing Background with hour and weather //
-
-
-
-const backgroundImages = {
-    'day': {
-        'Soleil': '../img/weather/sun-day.gif',
-        'Nuageux': '../img/weather/cloud-day.gif',
-        'Pluie': '../img/weather/rain-day.gif',
-        'Vent': '../img/weather/wind-day.gif',
-        'Brume': '../img/weather/fog-day.gif',
-        'Neige': '../img/weather/snow-day.gif',
-        'Tempete': '../img/weather/storm-day.gif'
-    },
-    'twilight': {
-        'Soleil': '../img/weather/sun-twilight.gif',
-        'Nuageux': '../img/weather/cloud-twilight.gif',
-        'Pluie': '../img/weather/rain-twilight.gif',
-        'Vent': '../img/weather/wind-twilight.gif',
-        'Brume': '../img/weather/fog-twilight.gif',
-        'Neige': '../img/weather/snow-twilight.gif',
-        'Tempete': '../img/weather/storm-twilight.gif'
-    },
-    'night': {
-        'Soleil': '../img/weather/sun-night.gif',
-        'Nuageux': '../img/weather/cloud-night.gif',
-        'Pluie': '../img/weather/rain-night.gif',
-        'Vent': '../img/weather/wind-night.gif',
-        'Brume': '../img/weather/fog-night.gif',
-        'Neige': '../img/weather/snow-night.gif',
-        'Tempete': '../img/weather/storm-night.gif'
-    }
-};
 
 
 
