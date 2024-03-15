e558ab83f0fef4bd782f21fda75a201074b5f433
Marco <zehow.zhw@gmail.com>
2024-03-15 14:08:59 +0100

    trying to fix img background bug

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ const updateUI = async (data) => {
         'day': {
             'Soleil': '../img/weather/sun-day.gif', 
             'Nuageux': 'https://raw.githubusercontent.com/Markohh9/Projet-API-METEO-/main/img/weather/cloud-day.gif', 
-            'Pluie': '../img/weather/rain-day.gif', 
+            'Pluie': 'https://raw.githubusercontent.com/Markohh9/Projet-API-METEO-/main/img/weather/rain-day.gif', 
             'Vent': '../img/weather/wind-day.gif', 
             'Brume': '../img/weather/fog-day.gif', 
             'Neige': '../img/weather/snow-day.gif', 
